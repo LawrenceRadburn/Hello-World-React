a512d7e979e3a9a6c40015f159360444eceb0c36
Lawrence Radburn <lawrence@radnet.ca>
2024-07-04 19:50:51 -0400

    feat: add content

--- hello-world-react/src/index.js
+++ hello-world-react/src/index.js
@@ -7,6 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const blueHeader = {
     color: 'blue'
 };
+const exampleExpression = 'This is an example expression.';
 
 root.render(
     <div>
@@ -21,5 +22,12 @@ root.render(
         <h2 style={blueHeader}>Internal Styling using React</h2>
         <p>Now, the h2 header tag above was styled internally.</p>
         <p>First, we created a variable using the const declaration. We called our variable blueHeader.</p>
-        <p>For example, const = blueheader. Next, we included the usual HTML syntax for styling HTML elements. So, color: 'blue'</p>
+        <p>For example, const = blueHeader. Next, we included the usual HTML syntax for styling HTML elements. So, color: 'blue'</p>
+        <br></br>
+        <h2>Expressions in React</h2>
+        <p>In React, we can store values inside variables. When we want to call the variable, we have to insert the variable inside
+            an expression. We use curly braces to utilize expressions.
+            For instance, the following text will call a variable and return its value using an expression.
+        </p>
+        <p><strong>{exampleExpression}</strong></p>
     </div>);
